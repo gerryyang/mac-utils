9d8d83e15bf612bf8d3efc2499225bad1f45c63c
gerryyang <diggwcdj@gmail.com>
2014-10-19 20:32:21 +0800

    add oc

---
+++ tools/Objective-C/src/02_helloworld.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by gerryyang on 14-10-19.
+//  Copyright (c) 2014Äê gerryyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    NSLog(@"Hello Objective-C, wcdj");
+    return 0;
+}
+
