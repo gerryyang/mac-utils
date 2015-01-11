efe71bad0d8dbdb540a47be59c35a726d5259d51
gerryyang <diggwcdj@gmail.com>
2015-01-11 20:33:11 +0800

    add get_public_ip.sh

---
+++ tools/network/get_public_ip.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+:<< wcdj
+use an outside party to tell you how it sees your server. You can do this by asking a specific server what your IP address is
+wcdj
+
+curl http://icanhazip.com/
+
+
