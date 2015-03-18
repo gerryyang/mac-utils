2c55a8e9ac549a8382d76dc46a951f9aafcdf474
gerryyang <diggwcdj@gmail.com>
2015-03-18 23:23:01 +0800

    add php

---
+++ tools/php/src/php/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>欢迎学习php!</title>
+    </head>
+	<body>
+       <p>
+       <?php 
+        // this is a comment
+        echo 'php learning, gerry';
+        echo 1024*4;
+       ?></p>
+       
+	</body>
+</html>
