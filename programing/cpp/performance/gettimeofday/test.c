bdf86cf27537a792ccf55bb55c64980fd7468581
gerryyang <diggwcdj@gmail.com>
2021-02-17 16:16:57 +0800

    add gettimeofday

--- programing/cpp/performance/gettimeofday/test.c
+++ programing/cpp/performance/gettimeofday/test.c
@@ -1,15 +1,24 @@
+#define _GNU_SOURCE        
+#include <sched.h>
 #include <stdint.h>
 #include <sys/time.h>
 #include <time.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
  * Checked against the Intel manual and GCC --hpreg
  *
  * volatile because the tsc always changes without the compiler knowing it.
  */
-#define RDTSC() ({ register uint64_t tim; __asm__ __volatile__( "rdtsc" : "=A" (tim) ); tim; })
+
+#ifdef __x86_64__
+#define RDTSC() ({ register uint32_t a,d; __asm__ __volatile__( "rdtsc" : "=a"(a), "=d"(d)); (((uint64_t)a)+(((uint64_t)d)<<32)); })
+#else
+#define RDTSC() ({ register uint64_t tim; __asm__ __volatile__( "rdtsc" : "=A"(tim)); tim; })
+#endif
+
 #define RDTSCP(aux) ({ register uint64_t tim; __asm__ __volatile__( "rdtscp" : "=A" (tim),"=c" (aux) ); tim; })
 
 // Static variables are initialized to 0
@@ -30,7 +39,6 @@ static inline int test_rdtscp()
 	return (edx & (1U<<27));
 }
 
-
 /*
  * Returns CPU clock in mhz
  * Notice that the function will cost the calling thread to sleep wait_us us
@@ -46,40 +54,43 @@ static inline int getcpuspeed_mhz(unsigned int wait_us)
 	tsc1 = RDTSC();
 
 	// If sleep failed, result is unexpected
-	if(nanosleep(&t, NULL))
+	if (nanosleep(&t, NULL))
+	{
 		return -1;
+	}
 
 	tsc2 = RDTSC();
 
-	return (tsc2 - tsc1)/(wait_us);
+	return (tsc2 - tsc1) / (wait_us);
 }
 
 static int getcpuspeed()
 {
 	static int speed = -1;
 
-	while(speed<100)
-		speed = getcpuspeed_mhz(50*1000);
+	while (speed < 100)
+	{
+		speed = getcpuspeed_mhz(50 * 1000);
+	}
 
 	return speed;
 }
 
-#define REGET_TIME_US	1
+#define REGET_TIME_US 1
 
 //#define getcpuspeed() 2000
 
-# define TIME_ADD_US(a, usec)                      \
-	do {                                             \
-		(a)->tv_usec += usec;                        \
-		while((a)->tv_usec >= 1000000)               \
-		{                                            \
-			(a)->tv_sec ++;                            \
-			(a)->tv_usec -= 1000000;                   \
-		}                                            \
+# define TIME_ADD_US(a, usec)                   \
+	do {                                        \
+		(a)->tv_usec += usec;                   \
+		while((a)->tv_usec >= 1000000)          \
+		{                                       \
+			(a)->tv_sec ++;                     \
+			(a)->tv_usec -= 1000000;            \
+		}                                       \
 	} while (0)
 
 
-//
 // Compile with -O2 to optimize mul/div instructions
 // The performance is restricted by 2 factors:
 //    1, the rdtsc instruction
@@ -94,24 +105,25 @@ static int my_gettimeofday(struct timeval *tv)
 {
 	uint64_t tick = 0;
 	// max_time_us = max_ticks / cpuspeed_mhz > RELOAD_TIME_US us
-	static unsigned int max_ticks = 2000*REGET_TIME_US;
+	static unsigned int max_ticks = 2000 * REGET_TIME_US;
 
-	if(walltime.tv_sec==0 || cpuspeed_mhz==0 ||
+	if (walltime.tv_sec == 0 || cpuspeed_mhz == 0 ||
 			// If we are on a different cpu with unsynchronized tsc,
 			// RDTSC() may be smaller than walltick
 			// in this case tick will be a negative number,
 			// whose unsigned value is much larger than max_ticks
-			(tick=RDTSC()-walltick) > max_ticks)
+			(tick = RDTSC() - walltick) > max_ticks)
 	{
-		if(tick==0 || cpuspeed_mhz==0)
+		if (tick == 0 || cpuspeed_mhz == 0)
 		{
 			cpuspeed_mhz = getcpuspeed();
-			max_ticks = cpuspeed_mhz*REGET_TIME_US;
+			max_ticks = cpuspeed_mhz * REGET_TIME_US;
 		}
 
 		gettimeofday(tv, NULL);
 		memcpy(&walltime, tv, sizeof(walltime));
 		walltick = RDTSC();
+
 		return 0;
 	}
 
@@ -125,9 +137,9 @@ static int my_gettimeofday(struct timeval *tv)
 		TIME_ADD_US(tv, t);
 	}
 #endif
+
 	return 0;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -135,43 +147,60 @@ int main(int argc, char *argv[])
 	unsigned int loops = 10000000;
 	struct timeval t1, t2, t3;
 
-	if(argc<2)
+	if (argc < 2)
 	{
 		printf("Please input an argument:\n\t1 compare time\n\t2 syscall\n\t3 fastcall\n");
 		return 0;
 	}
 
-	if(argc>2)
+	if (argc > 2)
+	{
 		loops = strtoul(argv[2], NULL, 10);
+	}
 
-	if(argv[1][0]=='1')
+	if (argv[1][0] == '1')
 	{
 		int spd = getcpuspeed();
 		uint64_t max = 0, diff = 0, nr_diff1 = 0, nr_diff10 = 0;
 		uint64_t tsc = RDTSC();
-		uint64_t af = 1;
 		int hastscp = 0;
 		uint32_t aux = 0;
 
-		if((hastscp=test_rdtscp()))
+		cpu_set_t set;
+		CPU_ZERO(&set);
+		CPU_SET(0, &set);
+
+		if ((hastscp = test_rdtscp()))
+		{
 			printf("This machine supports rdtscp.\n");
+		}
 		else
+		{
 			printf("This machine does not support rdtscp.\n");
+		}
 
-		if(hastscp)
+		if (hastscp)
+		{
+			printf("hastscp\n");
 			tsc = RDTSCP(aux);
+		}
 		else
+		{
+			printf("no hastscp\n");
 			tsc = RDTSC();
+		}
 
-		if(sched_setaffinity(0, sizeof(af), &af))
-			perror("failed to set affinity");
+		if (sched_setaffinity(0, sizeof(set), &set))
+		{
+			printf("failed to set affinity\n");
+		}
 
-		printf("tsc=%llu, aux=0x%x, cpu speed %d mhz\n", tsc, aux, spd);
-		printf("getspeed_010:%d\n", getcpuspeed_mhz(10*1000));
-		printf("getspeed_100:%d\n", getcpuspeed_mhz(100*1000));
-		printf("getspeed_500:%d\n", getcpuspeed_mhz(500*1000));
+		printf("tsc=%lu, aux=0x%x, cpu speed %d mhz\n", tsc, aux, spd);
+		printf("getspeed_010:%d\n", getcpuspeed_mhz(10 * 1000));
+		printf("getspeed_100:%d\n", getcpuspeed_mhz(100 * 1000));
+		printf("getspeed_500:%d\n", getcpuspeed_mhz(500 * 1000));
 
-		for(i=0; i<loops; i++)
+		for (i = 0; i < loops; ++i)
 		{
 #if 1
 			my_gettimeofday(&t1);
@@ -180,32 +209,43 @@ int main(int argc, char *argv[])
 #endif
 			gettimeofday(&t2, NULL);
 
-			if(timercmp(&t1, &t2, >))
+			if (timercmp(&t1, &t2, >))
+			{
 				timersub(&t1, &t2, &t3);
+			}
 			else
+			{
 				timersub(&t2, &t1, &t3);
+			}
 
-			//			printf("t1=%u.%06u\t", t1.tv_sec, t1.tv_usec);
-			//			printf("t2=%u.%06u\t", t2.tv_sec, t2.tv_usec);
-			//			printf("diff=%u.%06u\n", t3.tv_sec, t3.tv_usec);
+			//printf("t1=%u.%06u\t", t1.tv_sec, t1.tv_usec);
+			//printf("t2=%u.%06u\t", t2.tv_sec, t2.tv_usec);
+			//printf("diff=%u.%06u\n", t3.tv_sec, t3.tv_usec);
 
-			if(max<t3.tv_usec) max = t3.tv_usec;
-			if(t3.tv_usec>1) nr_diff1 ++;
-			if(t3.tv_usec>10) nr_diff10 ++;
+			if (max < t3.tv_usec) max = t3.tv_usec;
+			if (t3.tv_usec > 1) ++nr_diff1;
+			if (t3.tv_usec > 10) ++nr_diff10;
+
 			diff += t3.tv_usec;
-			//			if(i%20==0) usleep((i%10)<<5);
+			//if (i % 20 == 0) usleep((i%10)<<5);
 		}
-		printf("max diff=%llu, ave diff=%llu, diff1 count=%llu, diff10 count=%llu.\n", max, diff/i, nr_diff1, nr_diff10);
+		printf("max diff=%lu, ave diff=%lu, diff1 count=%lu, diff10 count=%lu.\n", max, diff/i, nr_diff1, nr_diff10);
 	}
-	else if(argv[1][0]=='2')
+	else if (argv[1][0] == '2')
 	{
-		for(i=0; i<loops; i++)
+		for (i = 0; i < loops; ++i)
+		{
 			gettimeofday(&t1, NULL);
+		}
 	}
-	else if(argv[1][0]=='3')
+	else if (argv[1][0] == '3')
 	{
-		for(i=0; i<loops; i++)
+		for (i = 0; i < loops; ++i)
+		{
 			my_gettimeofday(&t1);
+		}
 	}
+
+	return 0;
 }
 
