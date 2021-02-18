4f98abbe047e7afb86b3189ddcc81733a2782223
gerryyang <diggwcdj@gmail.com>
2021-02-18 13:09:03 +0800

    update

--- programing/cpp/atomic/atomic_test.c
+++ programing/cpp/atomic/atomic_test.c
@@ -5,7 +5,123 @@
 #include <sys/wait.h>
 #include "atomic_counter.h"
 
-// gcc -o ac atomic_test.c atomic_counter.c
+
+int test_error_inc()
+{
+	printf("test_error_inc:\n");
+
+	int i, j, p = 0;
+
+	atomic_counter ac;
+	atomic_counter_init(&ac, SHM_KEY, 0);
+	atomic_counter_set(&ac, 0);
+
+	for (i = 0; i < 9; ++i)
+	{
+		if (fork() == 0)
+		{
+			p = i + 1;
+			break;
+		}
+	}
+
+	for (j = 0; j < 1000000; ++j)
+	{
+		// error, no atomic
+		++(ac.ptr->dwCounter);
+	}
+	
+	if (p == 0)
+	{
+		while (wait(NULL) != -1);
+
+		// result
+		printf("dec%d: val=%u\n", p, ac.ptr->dwCounter);
+	}
+	else
+	{
+		printf("dec%d: val=%u\n", p, ac.ptr->dwCounter);
+		exit(0);
+	}
+
+	return 0;
+}
+
+int test_inc()
+{
+	printf("test_inc:\n");
+
+	int i, j, p = 0;
+
+	atomic_counter ac;
+	atomic_counter_init(&ac, SHM_KEY, 0);
+	atomic_counter_set(&ac, 0);
+
+	for (i = 0; i < 9; ++i)
+	{
+		if (fork() == 0)
+		{
+			p = i + 1;
+			break;
+		}
+	}
+
+	for (j = 0; j < 1000000; ++j)
+	{
+		atomic_counter_inc(&ac);
+	}
+
+	if (p == 0)
+	{
+		while (wait(NULL) != -1);
+		printf("inc%d: val=%u\n", p, ac.ptr->dwCounter);
+	}
+	else
+	{
+		printf("inc%d: val=%u\n", p, ac.ptr->dwCounter);
+		exit(0);
+	}
+
+	return 0;
+}
+
+int test_add()
+{
+	printf("test_add:\n");
+
+	int i, j, p = 0;
+
+	atomic_counter ac;
+	atomic_counter_init(&ac, SHM_KEY, 0);
+	atomic_counter_set(&ac, 0);
+
+	for (i = 0; i < 9; ++i)
+	{
+		if (fork() == 0)
+		{
+			p = i + 1;
+			break;
+		}
+	}
+
+	for (j = 0; j < 1000000; ++j)
+	{
+		atomic_counter_add(&ac, 1);
+	}
+	
+	printf("add%d: val=%u\n", p, ac.ptr->dwCounter);
+
+	if (p == 0)
+	{
+		while (wait(NULL) != -1);
+	}
+	else
+	{
+		exit(0);
+	}
+
+	return 0;
+}
 
 int test_dec()
 {
@@ -53,138 +169,15 @@ int test_dec()
 	return 0;
 }
 
-int test_inc()
-{
-	printf("test_inc:\n");
-
-	int i, j, p = 0;
-
-	atomic_counter ac;
-	atomic_counter_init(&ac, SHM_KEY, 0);
-	atomic_counter_set(&ac, 0);
-
-	for (i = 0; i < 9; ++i)
-	{
-		if (fork() == 0)
-		{
-			p = i + 1;
-			break;
-		}
-	}
-
-	for (j = 0; j < 1000000; ++j)
-	{
-		atomic_counter_inc(&ac);
-	}
-	
-	printf("inc%d: val1=%u\n", p, ac.ptr->dwCounter);
-
-	for (j = 0; j < 1000100; ++j)
-	{
-		atomic_counter_inc(&ac);
-	}
-	printf("inc%d: val2=%u\n", p, ac.ptr->dwCounter);
-
-	if (p == 0)
-	{
-		while (wait(NULL) != -1);
-	}
-	else
-	{
-		exit(0);
-	}
-
-	return 0;
-}
-
-int test_add()
-{
-	printf("test_add:\n");
-
-	int i, j, p = 0;
-
-	atomic_counter ac;
-	atomic_counter_init(&ac, SHM_KEY, 0);
-	atomic_counter_set(&ac, 0);
-
-	for (i = 0; i < 9; ++i)
-	{
-		if (fork() == 0)
-		{
-			p = i + 1;
-			break;
-		}
-	}
-
-	for (j = 0; j < 1000000; ++j)
-	{
-		atomic_counter_add(&ac, 1);
-	}
-	
-	printf("add%d: val=%u\n", p, ac.ptr->dwCounter);
-
-	if (p == 0)
-	{
-		while (wait(NULL) != -1);
-	}
-	else
-	{
-		exit(0);
-	}
-
-	return 0;
-}
-
-int test_error_inc()
-{
-	printf("test_error_inc:\n");
-
-	int i, j, p = 0;
-
-	atomic_counter ac;
-	atomic_counter_init(&ac, SHM_KEY, 0);
-	atomic_counter_set(&ac, 0);
-
-	for (i = 0; i < 9; ++i)
-	{
-		if (fork() == 0)
-		{
-			p = i + 1;
-			break;
-		}
-	}
-
-	for (j = 0; j < 1000000; ++j)
-	{
-		// error, no atomic
-		++(ac.ptr->dwCounter);
-	}
-	
-	if (p == 0)
-	{
-		while (wait(NULL) != -1);
-
-		// result
-		printf("dec%d: val=%u\n", p, ac.ptr->dwCounter);
-	}
-	else
-	{
-		printf("dec%d: val=%u\n", p, ac.ptr->dwCounter);
-		exit(0);
-	}
-
-	return 0;
-}
-
 int main(int argc, char *argv[])
 {
 	test_error_inc();
 
-	test_dec();
-
 	test_inc();
 
 	test_add();
+
+	test_dec();
 
 	return 0;
 }
