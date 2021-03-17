2d3a2bc297a0633145c93a65078c9dc0116eb78c
gerryyang <diggwcdj@gmail.com>
2021-03-17 15:01:30 +0800

    update demo

--- programing/cpp/shared_ptr/README.md
+++ programing/cpp/shared_ptr/README.md
@@ -2,23 +2,43 @@
 # Usage
 
 ```
-$ ./demo 
+$ ./demo
   Base::Base()
   Derived::Derived()
 Created a shared Derived (as a pointer to Base)
-  p.get() = 0x5580acf9fe80, p.use_count() = 1
-release ownership from main
-Shared ownership between 3 threads and released
+  p.get() = 0x5607c27ace80, p.use_count() = 1
+Copy a shared Derived (as a pointer to Base)
+  p.get() = 0x5607c27ace80, p.use_count() = 2
+Release ownership from main
+  p.get() = 0, p.use_count() = 0  p2.get() = 0x5607c27ace80, p2.use_count() = 1
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 2
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 4
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 4
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 5
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 7
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 7
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 8
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 9
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 10
+local pointer in a thread:
+  lp.get() = 0x5607c27ace80, lp.use_count() = 11
+Shared ownership between threads and released
 ownership from main:
-  p.get() = 0, p.use_count() = 0
-local pointer in a thread:
-  lp.get() = 0x5580acf9fe80, lp.use_count() = 4
-local pointer in a thread:
-  lp.get() = 0x5580acf9fe80, lp.use_count() = 3
-local pointer in a thread:
-  lp.get() = 0x5580acf9fe80, lp.use_count() = 2
+  p2.get() = 0x5607c27ace80, p2.use_count() = 1
+All threads completed, the last one deleted Derived
   Derived::~Derived()
   Base::~Base()
-All threads completed, the last one deleted Derived
 ```
 
+# Refer
+
+* https://en.cppreference.com/w/cpp/memory/shared_ptr
--- programing/cpp/shared_ptr/demo.cpp
+++ programing/cpp/shared_ptr/demo.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
+#include <vector>
 
 struct Base
 {
@@ -17,19 +18,20 @@ struct Derived: public Base
 	~Derived() { std::cout << "  Derived::~Derived()\n"; }
 };
 
-void thr(std::shared_ptr<Base> p)
+void thr(std::shared_ptr<Base>& p)
 {
-	std::this_thread::sleep_for(std::chrono::seconds(3));
+	// In multithreaded environment, the value returned by use_count is approximate
 
-	std::shared_ptr<Base> lp = p; // thread-safe, even though the shared use_count is incremented
+	//static std::mutex io_mutex;
+	//std::lock_guard<std::mutex> lk(io_mutex);
 
-	{
-		//static std::mutex io_mutex;
-		//std::lock_guard<std::mutex> lk(io_mutex);
-		std::cout << "local pointer in a thread:\n"
-			<< "  lp.get() = " << lp.get()
-			<< ", lp.use_count() = " << lp.use_count() << '\n';
-	}
+	auto lp = p; // thread-safe, even though the shared use_count is incremented
+
+	std::cout << "local pointer in a thread:\n"
+		<< "  lp.get() = " << lp.get()
+		<< ", lp.use_count() = " << lp.use_count() << '\n';
+
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 int main()
@@ -40,17 +42,40 @@ int main()
 		<< "  p.get() = " << p.get()
 		<< ", p.use_count() = " << p.use_count() << '\n';
 
-	std::thread t1(thr, p), t2(thr, p), t3(thr, p);
+	auto p2 = p;
 
-	p.reset(); // release ownership from main
-	std::cout << "release ownership from main\n";
-
-	std::cout << "Shared ownership between 3 threads and released\n"
-		<< "ownership from main:\n"
+	std::cout << "Copy a shared Derived (as a pointer to Base)\n"
 		<< "  p.get() = " << p.get()
 		<< ", p.use_count() = " << p.use_count() << '\n';
 
-	t1.join(); t2.join(); t3.join();
+	p.reset(); // release ownership from main
+
+	std::cout << "Release ownership from main\n"
+		<< "  p.get() = " << p.get()
+		<< ", p.use_count() = " << p.use_count()
+		<< "  p2.get() = " << p2.get()
+		<< ", p2.use_count() = " << p2.use_count() << '\n';
+
+	std::vector<std::thread> thr_vec;
+
+	for (int i = 0; i != 10; ++i)
+	{
+		// use std::ref, otherwise tmp object will be created, the result will not clear to see
+		std::thread t(thr, std::ref(p2));
+
+		thr_vec.push_back(std::move(t));
+	}
+
+	for (auto &t : thr_vec)
+	{
+		t.join();
+	}
+
+	std::cout << "Shared ownership between threads and released\n"
+		<< "ownership from main:\n"
+		<< "  p2.get() = " << p2.get()
+		<< ", p2.use_count() = " << p2.use_count() << '\n';
+
 
 	std::cout << "All threads completed, the last one deleted Derived\n";
 
