55206f4eddb3c2c3a2ebf3d038646b16553791b5
gerryyang <diggwcdj@gmail.com>
2021-04-19 10:35:55 +0800

    add protocol-buffers

---
+++ programing/protocol-buffers/tutorial/proto/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROTOCOL_DIR=./
+PROTOCOL_SRC_DIR=../src_protocol
+
+PROTOCOL_FILES="addressbook.proto"
+
+function Proc {
+	for file in $1
+	do
+		protoc --cpp_out=$PROTOCOL_SRC_DIR --proto_path=$PROTOCOL_DIR $file
+		if [ $? -ne 0 ]; then
+			echo "protoc $file failed"
+			exit 1
+		fi
+	done
+}
+
+Proc "$PROTOCOL_FILES"
+
+echo "ok"
