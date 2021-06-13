c6ed1ac02830a90a00840fba378f92719020f06a
gerryyang <diggwcdj@gmail.com>
2021-06-13 17:02:26 +0800

    add std_ABI_test

---
+++ programing/cpp/library/std_ABI_test/demo.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <unordered_map>
+#include "hello.h"
+
+int main() 
+{
+    std::cout << __FILE__ << ", __GNUC__(" << __GNUC__ << ") sizeof(std::unordered_map<int, int>) = " << sizeof(std::unordered_map<int, int>) << std::endl;
+    hello();
+    return 0;
+}
+
