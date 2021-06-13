60971ccdfd72b8add7cb040c78926f45d60b6b12
gerryyang <diggwcdj@gmail.com>
2021-06-14 00:26:50 +0800

    update std abi test

--- programing/cpp/library/std_ABI_test/demo.cc
+++ programing/cpp/library/std_ABI_test/demo.cc
@@ -2,10 +2,9 @@
 #include <unordered_map>
 #include "hello.h"
 
-int main() 
+int main()
 {
     std::cout << __FILE__ << ", __GNUC__(" << __GNUC__ << ") sizeof(std::unordered_map<int, int>) = " << sizeof(std::unordered_map<int, int>) << std::endl;
     hello();
     return 0;
 }
-
--- programing/cpp/library/std_ABI_test/hello.cc
+++ programing/cpp/library/std_ABI_test/hello.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <unordered_map>
 
-void hello() 
+void hello()
 {
     std::cout << __FILE__ << ", __GNUC__(" << __GNUC__ << ") sizeof(std::unordered_map<int, int>) = " << sizeof(std::unordered_map<int, int>) << std::endl;
-    std::unordered_map<int, int> m = { {1, 2}, {3, 4} };
-    for (auto& t : m) {
+    std::unordered_map<int, int> m = {{1, 2}, {3, 4}};
+    for (auto &t : m)
+    {
         std::cout << t.first << "," << t.second << std::endl;
     }
 }
-
